7f2fa04a614f7dde886b22c17a5103e6500f3d02
Han Yuan <yuanh@sci-pvm-yuanh.calicolabs.local>
2021-08-20 21:55:54 -0700

    update requirements

--- examples/train.sh
+++ examples/train.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-python ../bin/train.py --h5 ../data/processed/train_test_val.h5 --out_path ../data/output
+python ../bin/scbasset_train.py --h5 ../data/multiome_example/processed/train_test_val.h5 --out_path ../data/multiome_example/output
