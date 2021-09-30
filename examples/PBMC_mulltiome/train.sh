d430ae40dde6b3f4841122272e96940daca824a8
Han Yuan <yuanh@sci-pvm-yuanh.calicolabs.local>
2021-09-30 14:43:48 -0700

    tutorial-update

---
+++ examples/PBMC_mulltiome/train.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+python ../../bin/scbasset_train.py --h5 ../../data/download/pbmc_multiome_train_test_val.h5 --out_path ../../data/multiome_example/trained_model
--- examples/train.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-
-python ../bin/scbasset_train.py --h5 ../data/multiome_example/processed/train_test_val.h5 --out_path ../data/multiome_example/output
